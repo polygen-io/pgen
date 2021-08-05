fb38bfd0ac28d2c1ac4392c9cb1481ad34c81d30
thedavidmeister <thedavidmeister@gmail.com>
2021-08-05 12:26:05 +0400

    lint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,7 +17,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 100000
+            runs: 100000,
           },
         },
       },
